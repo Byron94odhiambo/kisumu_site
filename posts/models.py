cbd24bea449aa5f45d4cee88b30fcc60a49285fe
Byron94odhiambo <odhiambobyron39@gmail.com>
2019-05-05 19:59:48 +0300

    On branch master

--- posts/models.py
+++ posts/models.py
@@ -5,6 +5,7 @@ class Posts(models.Model):
   title = models.CharField(max_length=200)
   body = models.TextField()
   created_at = models.DateTimeField(default=datetime.now, blank=True)
+  images = models.ImageField(default='')
 
   def __str__(self):
     return self.title
