19963fa99f9c8f935100e0db237cf2fa0761cc53
Byron94odhiambo <odhiambobyron39@gmail.com>
2019-05-09 15:26:18 +0300

    On branch master

--- kisumu_site/settings.py
+++ kisumu_site/settings.py
@@ -20,6 +20,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
     'posts',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -28,6 +29,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
